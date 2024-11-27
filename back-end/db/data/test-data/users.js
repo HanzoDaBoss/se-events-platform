24ea817fde3c8bab72a9f02749b347361abb2998
HanzoDaBoss <hanif.uddz@gmail.com>
2024-11-27 02:02:03 +0000

    Copied dev data into test data

--- back-end/db/data/test-data/users.js
+++ back-end/db/data/test-data/users.js
@@ -7,31 +7,10 @@ module.exports = [
     role: "staff",
   },
   {
-    email: "jon.jones@company.com",
-    username: "bonesjones",
-    first_name: "Jon",
-    last_name: "Jones",
-    role: "user",
-  },
-  {
     email: "alex.volk@example.com",
     username: "volk145",
     first_name: "Alex",
     last_name: "Volkanovski",
-    role: "user",
-  },
-  {
-    email: "mia.taylor@example.com",
-    username: "miataylor",
-    first_name: "Mia",
-    last_name: "Taylor",
-    role: "user",
-  },
-  {
-    email: "sam.wilson@example.com",
-    username: "thefalcon",
-    first_name: "Sam",
-    last_name: "Wilson",
     role: "user",
   },
 ];
