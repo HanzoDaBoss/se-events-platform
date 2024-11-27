70e7142f2b6b6bc5e61e91e61d27633d992c45d5
HanzoDaBoss <hanif.uddz@gmail.com>
2024-11-27 00:45:32 +0000

    Successfully seeded user data into tables

--- back-end/db/seeds/seeds.js
+++ back-end/db/seeds/seeds.js
@@ -1,5 +1,10 @@
 const format = require("pg-format");
 const db = require("../connection");
+const { createClient } = require("@supabase/supabase-js");
+
+require("dotenv").config({
+  path: `${__dirname}/../../.env.supabase`,
+});
 
 const seed = ({ eventsData, usersData }) => {
   return db
@@ -8,18 +13,19 @@ const seed = ({ eventsData, usersData }) => {
       `DROP FUNCTION IF EXISTS public.handle_new_user;`;
     })
     .then(() => {
-      return db.query(`DROP TABLE IF EXISTS users;`);
+      return db.query(`DROP TABLE IF EXISTS users_events;`);
     })
     .then(() => {
       return db.query(`DROP TABLE IF EXISTS events;`);
     })
     .then(() => {
-      return db.query(`DROP TABLE IF EXISTS users_events;`);
+      return db.query(`DROP TABLE IF EXISTS users;`);
     })
     .then(() => {
       return db.query(`
       CREATE TABLE users (
-        id UUID NOT NULL REFERENCES auth.users ON delete cascade,
+        id UUID PRIMARY KEY REFERENCES auth.users ON delete cascade,
+        email VARCHAR NOT NULL,
         username VARCHAR NOT NULL,
         first_name VARCHAR NOT NULL,
         last_name VARCHAR NOT NULL,
@@ -33,7 +39,7 @@ const seed = ({ eventsData, usersData }) => {
         returns trigger as $$
         BEGIN
           INSERT INTO public.users (id, email, username, first_name, last_name, role)
-          VALUES (new.id, new.email, new.username, new.first_name, new.last_name, new.role);
+          VALUES (new.id, new.raw_user_meta_data ->> 'email', new.raw_user_meta_data ->> 'username', new.raw_user_meta_data ->> 'first_name', new.raw_user_meta_data ->> 'last_name', new.raw_user_meta_data ->> 'role');
           RETURN NEW;
         END;
         $$ language plpgsql security definer;
@@ -45,25 +51,96 @@ const seed = ({ eventsData, usersData }) => {
         AFTER INSERT ON auth.users FOR each row
         EXECUTE PROCEDURE public.handle_new_user ();
       `);
+    })
+    .then(() => {
+      return db.query(`
+      CREATE TABLE events (
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(100) NOT NULL,
+        location VARCHAR(255),
+        date DATE NOT NULL,
+        start_time TIMESTAMP NOT NULL,
+        end_time TIMESTAMP NOT NULL,
+        summary TEXT NOT NULL,
+        description TEXT NOT NULL,
+        image_dir VARCHAR NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        creator_id UUID REFERENCES users(id)
+      );
+      `);
+    })
+    .then(() => {
+      return db.query(`
+        CREATE TABLE users_events (
+        id SERIAL PRIMARY KEY,
+        user_id UUID REFERENCES auth.users(id) ON DELETE CASCADE,
+        event_id INT REFERENCES events(id) ON DELETE CASCADE,
+        signed_up_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        UNIQUE (user_id, event_id)
+      );
+      `);
+    })
+    .then(() => {
+      const supabaseAdmin = createClient(
+        process.env.SUPABASE_URL,
+        process.env.SERVICE_ROLE_KEY,
+        {
+          auth: {
+            autoRefreshToken: false,
+            persistSession: false,
+          },
+        }
+      );
+      const createUsersPromises = usersData.map((user) => {
+        return supabaseAdmin.auth.admin.createUser({
+          email: user.email,
+          email_confirm: true,
+          password: "password123",
+          user_metadata: {
+            email: user.email,
+            username: user.username,
+            first_name: user.first_name,
+            last_name: user.last_name,
+            role: user.role,
+          },
+        });
+      });
+      return Promise.all(createUsersPromises);
+    })
+    .then(() => {
+      const insertEventsQueryStr = format(
+        `INSERT INTO events (title, location, date, start_time, end_time, summary, description, image_dir, created_at, updated_at, creator_id) VALUES %L`,
+        eventsData.map(
+          ({
+            title,
+            location,
+            date,
+            start_time,
+            end_time,
+            summary,
+            description,
+            image_dir,
+            created_at,
+            updated_at,
+            creator_id,
+          }) => [
+            title,
+            location,
+            date,
+            start_time,
+            end_time,
+            summary,
+            description,
+            image_dir,
+            created_at,
+            updated_at,
+            creator_id,
+          ]
+        )
+      );
+      return db.query(insertEventsQueryStr);
     });
-  // .then(() => {
-  //   return db.query(`
-  //     CREATE TABLE events (
-  //       id SERIAL PRIMARY KEY,
-  //       title VARCHAR(100) NOT NULL,
-  //       location VARCHAR(255),
-  //       date DATE NOT NULL,
-  //       start_time TIMESTAMP NOT NULL,
-  //       end_time TIMESTAMP NOT NULL,
-  //       summary TEXT NOT NULL,
-  //       description TEXT NOT NULL,
-  //       image_dir VARCHAR NOT NULL,
-  //       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  //       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  //       creator_id INT REFERENCES users(id)
-  //     )
-  //     `);
-  // });
 };
 
 module.exports = seed;
