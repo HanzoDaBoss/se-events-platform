f8ae5416c1698fe611121f6efbed47cc348ef73d
HanzoDaBoss <hanif.uddz@gmail.com>
2024-12-05 01:16:04 +0000

    Created login component

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,9 +2,15 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Login from "./components/Login";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <>hi</>;
+  return (
+    <>
+      <Login></Login>
+    </>
+  );
 }
 
 export default App;
