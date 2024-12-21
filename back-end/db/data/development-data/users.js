de4e01a9db7670a03042c7b43ee39cad89bb42c9
HanzoDaBoss <hanif.uddz@gmail.com>
2024-12-21 03:12:29 +0000

    Updated test users data and removed Button component

--- back-end/db/data/development-data/users.js
+++ back-end/db/data/development-data/users.js
@@ -1,13 +1,13 @@
 module.exports = [
   {
-    email: "test-staff@test.com",
+    email: "test-staff@wentevent.com",
     username: "Staff",
     first_name: "Forename",
     last_name: "Surname",
     role: "staff",
   },
   {
-    email: "test-user@test.com",
+    email: "test-user@wentevent.com",
     username: "User",
     first_name: "Forename",
     last_name: "Surname",
